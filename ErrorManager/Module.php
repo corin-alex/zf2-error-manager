727dd1be11405124d28c4b06862ebcd7d8d4d56b
Corin Alexandru <corin.alex@gmail.com>
2017-03-28 21:31:23 +0200

    new style, stopping execution after showing error, letting zend handle exceptions

--- ErrorManager/Module.php
+++ ErrorManager/Module.php
@@ -2,6 +2,7 @@
 /**
  * Simple Error Manager for Zend Framework 2
  *
+ * @version   1.1
  * @link      http://github.com/corin-alex/zf2-error-manager
  * @copyright Copyright (c) 2017 Corin Alexandru
  * @license   MIT
@@ -34,24 +35,33 @@ class Module implements BootstrapListenerInterface
     public function handleError(MvcEvent $ev) {
         $ex = $ev->getParam('exception');
 
-        echo "<style>
-         .errPane { background: #DDD; border: 1px solid #EEE; border-radius:0.25em; padding: 1em; }
-         .errMsg { color: #F00; }
-         .errWrap { margin: 5em 1em; padding: 1em; background: #CCC; color:#333; border-radius:0.25em;}
-         .errTracePane { background: #DDD; margin-bottom: 1em; margin-right:1em;}
-         .errTracePane pre { border:0; background:0; color #333; margin: 0; display: inline-block; vertical-align: middle;}
-         .errTraceWrap { height: 300px; overflow-y: scroll; }
-         </style>";
+        // We let zend handle exceptions
+        if (get_class($ex) == "Exception") return;
 
-        echo '<div class="errWrap"><h2>Too bad... =(</h2><div class="errPane">';
-        echo '<span class="errMsg">' . $ex->getMessage() . '</span>' .
-             "<br>In file : " . $ex->getFile() .
-             "<br>Line : " . $ex->getLine();
+          echo "<!DOCTYPE html><html><head><title>An error has occured</title><style>
+            .errWrap { padding: 0; margin: 1em; border-radius: 0.25em;  background: #FAFAFA; box-shadow: 0 0 3px rgba(0,0,0,0.2); font-family: Helvetiva, Verdana, SansSerif; }
+            .errTitle { background-color: #DA4453; height: 2.5em; line-height: 2.5em; border-top-left-radius: 0.25em;  border-top-right-radius: 0.25em; color: white; padding: 0 1em; }
+            .traceTitle { background-color: #F6BB42; }
+            .traceContent { height: 300px; max-height: 300px; min-height: 300px; overflow-y: scroll; }
+            .errFooter { text-align: center; color: #AAA; font-size: 0.8em; }
+            a.errLink { color: #AAA; text-decoration: none; }
+            a.errLink:hover { color: #666; }
+            .errBody { padding: 1em; }
+            .errBody em { font-size: 0.8em; color: #666;}
+            .errBody span { color: #A00 }
+            .errTracePane { margin: 1em 0; padding:1em; background-color: #FEFEFE; border: 1px solid #FFF; }
+            .errTracePane pre { border:0; background:0; color #333; margin: 0; display: inline-block; vertical-align: middle;}
+            </style></head><body>";
 
-        echo '</div><br><h3>Trace</h3><div class="errTraceWrap">';
+        echo '<div class="errWrap"><div class="errTitle">Error =(</div><div class="errBody">';
+        echo '<span>' . $ex->getMessage() . '</span>' .
+             "<em><br>In file : " . $ex->getFile() .
+             "<br>Line : " . $ex->getLine() . '</em>';
+
+        echo '<br><br><div class="errWrap"><div class="errTitle traceTitle">Trace</div><div class="errBody traceContent">';
 
         foreach ($ex->getTrace() as $trace) {
-            echo '<div class="errPane errTracePane">';
+            echo '<div class="errTracePane">';
             if (!empty($trace['file'])) echo "File : " . $trace['file'] . "<br>";
             if (!empty($trace['line'])) echo "Line : " . $trace['line'] . "<br>";
             if (!empty($trace['function'])) echo "Function : " . $trace['function'] . "<br>";
@@ -62,7 +72,9 @@ class Module implements BootstrapListenerInterface
             $this->errListArgs($trace['args']);
             echo "</div>";
         }
-        echo "</div></div>";
+        echo '</div></div></div><div class="errFooter"><a class="errLink" href="https://github.com/corin-alex/zf2-error-manager" target="_blank">ZF2 Error Manager 1.1 by Qhorin</a></div><br></div>';
+        echo "</body></html>";
+        exit;
     }
 
     /**
